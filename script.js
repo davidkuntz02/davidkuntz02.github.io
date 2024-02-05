648158b4ed0350bf5f0b72999967150fd62781b2
David <davidkuntz02@gmail.com>
2024-02-05 12:58:36 -0700

    better

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+console.log("im in");
+const toggle = document.getElementById("display-toggle");
+const body = document.getElementsByTagName("body")[0];
 
+console.log("good so far");
+toggle.onclick = function(){
+    console.log("script worked");
+    body.classList.toggle("dark");
+    toggle.innerText == "Light mode"?
+    toggle.innerText = "Dark mode": toggle.innerText = "Light mode";
+}
