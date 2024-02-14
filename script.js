1bd5b242c5027e984d94ef51c58ea7ff98a8b6a8
David <davidkuntz02@gmail.com>
2024-02-13 21:12:38 -0700

    almost done

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 console.log("im in");
-const toggle = document.getElementById("display-toggle");
-const body = document.getElementsByTagName("body")[0];
+const toggel = document.getElementById("display-toggle");
+const body = document.body;
 
 console.log("good so far");
-toggle.onclick = function(){
+toggel.onclick = function(){
     console.log("script worked");
     body.classList.toggle("dark");
-    toggle.innerText == "Light mode"?
-    toggle.innerText = "Dark mode": toggle.innerText = "Light mode";
+    toggel.innerText == "Light mode"?
+    toggel.innerText = "Dark mode": toggel.innerText = "Light mode";
 }
