944cd06180fc4b7a6cbacb6c04ea419f46f5d7f1
David Kuntz <123694850+davidkuntz02@users.noreply.github.com>
2025-03-04 14:38:58 -0700

    update personal page

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const body = document.body;
 
 console.log("good so far");
 toggel.onclick = function(){
-    console.log("script worked");
     body.classList.toggle("dark");
     toggel.innerText == "Light mode"?
     toggel.innerText = "Dark mode": toggel.innerText = "Light mode";
